fc2de836f956cf1f36270c97d66b2dab82577152
Rousslan Fakhreddine <rousslanmcs@gmail.com>
2025-11-03 02:17:52 +0100

    API working with CORS fix

--- server.js
+++ server.js
@@ -3,25 +3,23 @@ const app = express();
 
 app.use(express.json());
 
-// CORS temporaire - permet tout
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
-
+// Pas de CORS du tout - juste pour test
 app.get('/api/load', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
   res.json({ success: false, message: "API works!" });
 });
 
 app.post('/api/save', (req, res) => {
-  console.log('Save request:', req.body);
+  res.set('Access-Control-Allow-Origin', '*');
+  console.log('Save:', req.body);
   res.json({ success: true });
+});
+
+app.options('*', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
+  res.sendStatus(200);
 });
 
 const port = process.env.PORT || 3000;
