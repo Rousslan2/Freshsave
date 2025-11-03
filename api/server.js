954b736bf62cf0634cf725d59d9b80af8fe9cd1d
Rousslan Fakhreddine <rousslanmcs@gmail.com>
2025-11-03 03:08:24 +0100

    Fix Vercel API structure

--- api/server.js
+++ api/server.js
@@ -3,26 +3,26 @@ const app = express();
 
 app.use(express.json());
 
-// Pas de CORS du tout - juste pour test
+// CORS pour WebGL
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+// Vos routes API
 app.get('/api/load', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
   res.json({ success: false, message: "API works!" });
 });
 
 app.post('/api/save', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
   console.log('Save:', req.body);
   res.json({ success: true });
 });
 
-app.options('*', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.set('Access-Control-Allow-Headers', 'Content-Type');
-  res.sendStatus(200);
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('API running on port ' + port);
-});
+module.exports = app;
